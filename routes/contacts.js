77e54301f0befc46fb045340fc9ca154e7c2561d
Sean Bradbury <sean.bradbury@remarkable.net>
2022-07-21 08:45:01 +0100

    update contact route updated

--- routes/contacts.js
+++ routes/contacts.js
@@ -46,7 +46,7 @@ router.post(
 
       const contact = await newContact.save();
 
-      req.json(contact);
+      res.json(contact);
     } catch (err) {
       console.log(err.msg);
 
@@ -58,8 +58,40 @@ router.post(
 //@route    PUT api/contacts
 //@desc     Update contact
 //@access   Private
-router.put('/:id', (req, res) => {
-  res.send('Update contact');
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Build contact object
+  const contactFields = {};
+
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact)
+      return res.status(404).json({ msg: 'Contact not found' });
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.log(err.msg);
+
+    res.status(500).send('Server Error');
+  }
 });
 
 //@route    DELETE api/contacts
